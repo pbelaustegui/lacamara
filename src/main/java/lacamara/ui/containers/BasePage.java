48cfd3e45c46bda19c5380fa63643ad9b2c2c20c
unknown <pablo@win-desdemona.(none)>
2012-03-24 01:51:07 -0300

    un poco de estilo

--- src/main/java/lacamara/ui/containers/BasePage.java
+++ src/main/java/lacamara/ui/containers/BasePage.java
@@ -15,8 +15,9 @@ import org.apache.wicket.markup.html.resources.StyleSheetReference;
 public class BasePage extends WebPage{
 
     public BasePage(){
+        add(new StyleSheetReference("style", BasePage.class, "styles.css"));
         add(new Label("title", "La cámara de Mónica"));
         add(new Label("headerMessage", "La cámara de Mónica (En construcción)"));
-        add(new Label("footer", "............."));
+        add(new Label("footer", "."));
     }
 }
--- src/main/java/lacamara/ui/containers/HomePage.java
+++ src/main/java/lacamara/ui/containers/HomePage.java
@@ -12,6 +12,6 @@ import org.apache.wicket.markup.html.basic.Label;
 public class HomePage extends BasePage{
 
     public HomePage(){
-        add(new Label("bodyMessage", "..."));
+        add(new Label("bodyMessage", "."));
     }
 }
